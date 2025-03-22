b38532770d612e14baa4f9f3161bf80087d2604d
Volodymyr <volodymyr.shyrmanov@nixs.com>
2025-03-22 15:29:00 +0100

    Add initial project setup with .gitignore, global.json, and README instructions

---
+++ PromptingWithMsExtensions/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.AI;
+
+IChatClient client = new OllamaChatClient(
+    new Uri("http://localhost:11434"), "phi4");
+string? prompt;
+do
+{
+    Console.WriteLine("Enter your prompt (or 'exit' to quit):");
+    prompt = Console.ReadLine();
+    if (string.IsNullOrEmpty(prompt))
+    {
+        Console.WriteLine("Please enter your prompt.");
+        continue;
+    }
+
+    Console.WriteLine(await client.GetResponseAsync(prompt));
+} while (prompt != "exit");
